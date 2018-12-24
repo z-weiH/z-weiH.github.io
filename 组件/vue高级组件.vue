338248a68426d2865c461cc5b7b90eb6a91ae247
zengwei <18279047775@163.com>
2018-12-24 14:25:04 +0800

    增加 vue高级组件示例代码

---
+++ 组件/vue高级组件.vue
@@ -0,0 +1,23 @@
+<template>
+  <!-- 绑定所有属性 以及 事件 -->
+  <el-upload v-bind="$attrs" v-on="$listeners">
+    <!-- 默认插槽 -->
+    <slot></slot>
+    <!-- 其他插槽 -->
+    <template v-if="key !== 'default'" :slot="key" v-for="(item,key) in $slots">
+      <slot :name="key"></slot>
+    </template>
+  </el-upload>
+</template>
+
+<script>
+  export default {
+    mounted() {
+      console.log(this);
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
